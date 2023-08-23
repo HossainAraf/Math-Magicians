54d290568bdf7e94e327d6b86c632858ef37d0ec
Hossain <arafat.kd99@gmail.com>
2023-08-23 17:08:37 +0100

    Update Calculator function

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,31 @@
 import React from 'react';
 import '../styles/calculator.css';
+import Button from './Button';
 
 const Calculator = () => (
   <div className="calculator-wrapper">
     <div className="calculator">
       <div className="display">0</div>
       <div className="operator">
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button className="operator-right-column" type="button">/</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button className="operator-right-column" type="button">x</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button className="operator-right-column" type="button">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button className="operator-right-column" type="button">+</button>
-        <button className="zero-btn" type="button">0</button>
-        <button type="button">.</button>
-        <button className="operator-right-column" type="button">=</button>
-
+        <Button label="AC" />
+        <Button label="+/-" />
+        <Button label="%" />
+        <Button className="operator-right-column" label="/" />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button className="operator-right-column" label="x" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button className="operator-right-column" label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button className="operator-right-column" label="+" />
+        <Button className="zero-btn" label="0" />
+        <Button label="." />
+        <Button className="operator-right-column" label="=" />
       </div>
     </div>
   </div>
