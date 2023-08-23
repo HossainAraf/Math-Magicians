d42c7b1719d8c571a59d9d56969255add6396a99
Hossain <arafat.kd99@gmail.com>
2023-08-23 18:21:36 +0100

    Add App function to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import './index.css';
 import App from './App';
 import logo from './Assets/logo-2.png';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    {/* Include your logo or other elements here */}
     <img id="logo" src={logo} alt="Logo" />
     <App />
   </React.StrictMode>,
-  document.getElementById('root'),
 );
