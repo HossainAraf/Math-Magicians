aacda55c0935c2f5184c91e43bcdad87fd4f996a
Hossain <arafat.kd99@gmail.com>
2023-08-23 17:27:47 +0100

    Add logo

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Calculator from './components/Calculator';
+import logo from './Assets/logo-2.png';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <img src={logo} alt="Logo" />
     <Calculator />
   </React.StrictMode>,
 );
