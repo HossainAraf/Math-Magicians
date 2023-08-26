8a9d21deb0914708a1ec3af745faa005c79bbdb2
Hossain <arafat.kd99@gmail.com>
2023-08-26 15:42:32 +0100

    Update Quote component

--- src/components/DisplayQuote.js
+++ src/components/DisplayQuote.js
@@ -1,50 +1,65 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import '../styles/display-quote.css';
 
-const JokeComponent = () => {
-  const [joke, setJoke] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+const uniqueID = 'Ixgm2cYnlo7wGSvmiMQCVvfUJ6kVeTvgNZShaXEC';
+
+function Quote() {
+  const [quoteText, setQuoteText] = useState('');
+  const [authorText, setAuthor] = useState('');
+  const [categoryText, setCategory] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   useEffect(() => {
-    const myAPIKey = 'Ixgm2cYnlo7wGSvmiMQCVvfUJ6kVeTvgNZShaXEC';
-    const apiURL = 'https://api.api-ninjas.com/v1/dadjokes?limit=1';
-
-    const options = {
-      method: 'GET',
-      headers: { 'X-Api-Key': myAPIKey },
-    };
-
-    const fetchData = async () => {
+    const fetchQuote = async () => {
+      setIsLoading(true);
       try {
-        const response = await fetch(apiURL, options);
+        const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
+          headers: {
+            'X-Api-Key': uniqueID,
+          },
+        });
+        if (!response.ok) {
+          throw new Error(
+            `API request failed with status code ${response.status}.`,
+          );
+        }
         const data = await response.json();
-        setJoke(data[0].joke);
+        setQuoteText(data[0].quote);
+        setAuthor(data[0].author);
+        setCategory(data[0].category);
       } catch (error) {
-        setError(true);
+        setHasError(true);
       }
-      setLoading(false);
+      setIsLoading(false);
     };
+    fetchQuote();
+  }, []); // Empty dependency array to run only once on initial component mount
 
-    fetchData(); // Call the function to fetch data when the component mounts
-  }, []);
   return (
-    <div>
-      {loading && <p>Loading...</p>}
-      {error && (
-        <div>
-          <p>Error:</p>
-          {/* <p>{error}</p> */}
-        </div>
+    <div id="display-quote">
+      <h4>Quotes of the Day:</h4>
+      {isLoading && <p className="loading">Loading Quote...</p>}
+      {hasError && <p className="error">Something wrong !</p>}
+
+      {quoteText && (
+        <p className="quote">
+          &quot;
+          {quoteText}
+          &quot;
+        </p>
       )}
-      {joke && (
-        <div id="display-joke">
-          <h4>Joke of the Day:</h4>
-          <p>{joke}</p>
-        </div>
+      <hr />
+      <br />
+      {authorText && (
+        <p className="author-category">
+          {authorText}
+          ,&nbsp;
+          {categoryText}
+        </p>
       )}
     </div>
   );
-};
+}
 
-export default JokeComponent;
+export default Quote;
