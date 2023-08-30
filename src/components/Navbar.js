8a428efbbcbc052faa2657e0f631673d3f717510
Hossain <arafat.kd99@gmail.com>
2023-08-30 12:22:30 +0100

    Update navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NavStyles from '../styles/Navbar.module.css';
 
 const Navbar = () => (
-  <nav>
-    <ul>
+  <nav className={NavStyles.nav}>
+    <h3>Math Magicians</h3>
+    <ul className={NavStyles.ul}>
       <li><Link to="/">Home</Link></li>
-      <li><Link to="/Calculator">calculator</Link></li>
+      <li><Link to="/calculator">Calculator</Link></li>
       <li><Link to="/quote">Quote</Link></li>
     </ul>
   </nav>
