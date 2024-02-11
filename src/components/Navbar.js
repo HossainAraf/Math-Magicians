3b273e3faf3b58ea117055af359ac05e861f2c1e
Hossain <arafat.kd99@gmail.com>
2024-02-11 18:46:54 +0100

    Update logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import NavStyles from '../styles/Navbar.module.css';
+import logo from '../Assets/logo.png';
 
 const Navbar = () => (
   <nav className={NavStyles.nav}>
+    <img src={logo} alt="Logo" />
     <h1>Math Magicians</h1>
     <ul className={NavStyles.ul}>
       <li><Link to="/">Home</Link></li>
